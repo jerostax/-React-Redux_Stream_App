b78d305a1374e6f51c2e07a2e67a3d05758fa130
jerostax <jeremy.geneste@gmail.com>
2019-05-23 15:52:52 +0200

    [ CONFIG ] Rendered the Authentification status

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '629812139829-nccuf03a5caukfosrpmdqr1bdhfg874o.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get()})
       });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Je ne sais pas si je suis connecté</div>
+    } else if (this.state.isSignedIn) {
+      return <div>Je suis connecté</div>
+    } else {
+      return <div>Je ne suis pas connecté</div>
+    }
+  }
   render() {
-    return(
-      <div>Google Auth</div>
+    return (
+      <div>{this.renderAuthButton()}</div>
     );
   }
 }
