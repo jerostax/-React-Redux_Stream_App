ac1f937634ac2df719d46f9952885a269897e0ec
jerostax <jeremy.geneste@gmail.com>
2019-05-23 11:57:39 +0200

    [ FEATURE ] Added Header component and connected it

---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+    <Link to="/" className="item">
+      Stream App
+    </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          Tous les streams
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
