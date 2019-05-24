2b376bbf29d1b1019cd4c2e38631859293c2b2b5
jerostax <jeremy.geneste@gmail.com>
2019-05-24 17:09:16 +0200

    [ FEATURE ] Added validation form handler and error messages

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,25 +2,47 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
+    // console.log(meta);
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues)
   }
   render() {
 
     return (
-      <form className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
         <Field name="title" component={this.renderInput} label="Entrez le titre" />
         <Field name="description" component={this.renderInput} label="Entrez la déscription" />
+        <button className="ui button primary">Créer un Stream</button>
       </form>
     );
   }
 }
 
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'Vous devez entrer un titre';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'Vous devez entrer une déscription';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
-  form: 'streamCreate'
+  form: 'streamCreate',
+  validate
 })(StreamCreate);
