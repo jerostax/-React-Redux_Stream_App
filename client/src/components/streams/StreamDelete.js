1094b073008fb2e84ec874c118dbe872e4276ac7
jerostax <jeremy.geneste@gmail.com>
2019-05-28 09:55:48 +0200

    [ FEATURE ] Refacto StreamDelete fom function to class component to fetch the stream to delete

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Supprimer</button>
-      <button className="ui button">Annuler</button>
-    </React.Fragment>
-  );
-  return (
-    <div>
-      StreamDelete
-      <Modal 
-        title="Supprimer ce Stream"
-        content="Êtes vous sur de vouloir supprimer ce Stream?"
-        actions={actions}
-        onDismiss={() =>  history.push('/')}
-      />
-    </div>
-  );
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Supprimer</button>
+        <button className="ui button">Annuler</button>
+      </React.Fragment>
+    );
+  }
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Supprimer ce Stream"
+          content="Êtes vous sur de vouloir supprimer ce Stream?"
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+        />
+      </div>
+    );
+  }
 }
 
-export default StreamDelete;
+export default connect(null, { fetchStream })(StreamDelete);
